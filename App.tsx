dcf41a95feb44165d5df32ea3b348b4797f62ce3
Osman.Ibrahiem <Osman.Ibrahiem187@gmail.com>
2026-07-07 13:59:32 +0300

    feat: add QueryClientProvider setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppNavigator from '@navigation/AppNavigator';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <AppNavigator />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <AppNavigator />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
